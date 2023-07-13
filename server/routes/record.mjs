95a8693e2934e623fa7ffc9f8a9bae3b2692acea
anweshapalit99 <anweshapalit123@gmail.com>
2023-07-13 14:49:41 +0530

    Adding changes to record

--- server/routes/record.mjs
+++ server/routes/record.mjs
@@ -9,13 +9,14 @@ const recordRoutes = express.Router();
 import dbo from "../db/conn.mjs";
 
 // This help convert the id from string to ObjectId for the _id.
-//const ObjectId = new mongodb.ObjectId();
+
+// Creating single instance of db and collection
 const db_connect = await dbo.getDb();
-const collection = db_connect.collection("record");
+const collection_record = db_connect.collection("record");
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/").get(async function (req, res) {
-  let results = await collection.find({}).limit(50).toArray();
+  let results = await collection_record.find({}).limit(50).toArray();
   //console.log(results);
   res.send(results).status(200);
 });
@@ -27,11 +28,11 @@ recordRoutes.post("/add", async (req, res) => {
     lastName: req.body.last,
     avatarUrl: req.body.avatar,
     notes: req.body.notes,
-    date: new Date(),
+    dateCreated: new Date(),
   };
-  let result = await collection.insertOne(newDocument);
+  let result = await collection_record.insertOne(newDocument);
   /* Adding the count in the response */
-  collection.estimatedDocumentCount().then((count) => {
+  collection_record.estimatedDocumentCount().then((count) => {
     result.count = count;
     res.status(200).send(result);
   });
@@ -40,12 +41,10 @@ recordRoutes.post("/add", async (req, res) => {
 // This section will help you get a single record by id
 recordRoutes.route("/:id").get(function (req, res) {
   let myquery = { _id: new ObjectId(req.params.id) };
-  //console.log(myquery);
-  collection
+  collection_record
     .findOne(myquery)
     .then((result) => {
       if (result) {
-        //console.log(`Successfully found document: ${result}.`);
         res.status(200).send(result);
       } else {
         console.log("No document matches the provided query.");
@@ -64,10 +63,10 @@ recordRoutes.route("/update/:id").post(function (req, response) {
       lastName: req.body.last,
       avatarUrl: req.body.avatar,
       notes: req.body.notes,
-      date: new Date(),
+      dateUpdated: new Date(),
     },
   };
-  collection.updateOne(myquery, newvalues, function (err, res) {
+  collection_record.updateOne(myquery, newvalues, function (err, res) {
     if (err) throw err;
     console.log("1 document updated");
     response.json(res);
@@ -75,10 +74,9 @@ recordRoutes.route("/update/:id").post(function (req, response) {
 });
 
 // This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
-  let db_connect = dbo.getDb();
+recordRoutes.route("/delete/:id").delete((req, response) => {
   let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+  collection_record.deleteOne(myquery, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
     response.json(obj);
